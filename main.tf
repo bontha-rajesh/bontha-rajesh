64ac88d3c2485180164cdcc97e0650235ec1e994
bontha-rajesh <138701140+bontha-rajesh@users.noreply.github.com>
2025-03-27 17:37:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,3 +5,8 @@ region = "ap-south-2"
 resource "aws_s3_bucket" "bucket" {
                  bucket = "ccitgitbucket25"
 }
+resource "aws_s3_bucket" "bucket1" {
+                 bucket = "ccitgitbucket2025"
+}
+
+
